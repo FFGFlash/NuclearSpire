a4f252747a24bbd33d11a548443f0861bfa5a3f5
FFGFlash <tayld.ainjastudios@gmail.com>
2019-02-01 21:00:58 -0600

    update

--- src/com/flash/ns/CrystalMod.java
+++ src/com/flash/ns/CrystalMod.java
@@ -18,108 +18,127 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.localization.RelicStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.BaseMod;
 
-public class CrystalMod implements CharacterMod {
-	public static final Logger logger = LogManager.getLogger(NuclearSpireMod.class.getName());
+public class CrystalMod implements CharacterMod
+{
+    public static final Logger logger = LogManager.getLogger(NuclearSpireMod.class.getName());
 
-	private static final Color PURPLE = CardHelper.getColor(139.0f, 0.0f, 139.0f);
+    private static final Color PURPLE = CardHelper.getColor(139.0f, 0.0f, 139.0f);
 
-	private static final String ATTACK = "images/characters/crystal/card/512/bg_attack.png";
-	private static final String SKILL = "images/characters/crystal/card/512/bg_skill.png";
-	private static final String POWER = "images/characters/crystal/card/512/bg_power.png";
-	private static final String ENERGY_ORB = "images/characters/crystal/card/512/card_orb.png";
-	private static final String CARD_ENERGY_ORB = "images/characters/crystal/card/512/card_small_orb.png";
+    private static final String ATTACK = "images/characters/crystal/card/512/bg_attack.png";
+    private static final String SKILL = "images/characters/crystal/card/512/bg_skill.png";
+    private static final String POWER = "images/characters/crystal/card/512/bg_power.png";
+    private static final String ENERGY_ORB = "images/characters/crystal/card/512/card_orb.png";
+    private static final String CARD_ENERGY_ORB = "images/characters/crystal/card/512/card_small_orb.png";
 
-	private static final String ATTACK_PORTRAIT = "images/characters/crystal/card/1024/bg_attack.png";
-	private static final String SKILL_PORTRAIT = "images/characters/crystal/card/1024/bg_skill.png";
-	private static final String POWER_PORTRAIT = "images/characters/crystal/card/1024/bg_power.png";
-	private static final String ENERGY_ORB_PORTRAIT = "images/characters/crystal/card/1024/card_orb.png";
+    private static final String ATTACK_PORTRAIT = "images/characters/crystal/card/1024/bg_attack.png";
+    private static final String SKILL_PORTRAIT = "images/characters/crystal/card/1024/bg_skill.png";
+    private static final String POWER_PORTRAIT = "images/characters/crystal/card/1024/bg_power.png";
+    private static final String ENERGY_ORB_PORTRAIT = "images/characters/crystal/card/1024/card_orb.png";
 
-	private static final String BUTTON = "images/characters/crystal/button.png";
-	private static final String PORTRAIT = "images/characters/crystal/portrait.jpg";
+    private static final String BUTTON = "images/characters/crystal/button.png";
+    private static final String PORTRAIT = "images/characters/crystal/portrait.jpg";
 
-	private static final String CARD_STRINGS = "localization/crystal_cards.json";
-	private static final String RELIC_STRINGS = "localization/crystal_relics.json";
+    private static final String CARD_STRINGS = "localization/crystal_cards.json";
+    private static final String RELIC_STRINGS = "localization/crystal_relics.json";
+    private static final String POWER_STRINGS = "localization/crystal_powers.json";
 
-	public CrystalMod() {
-		logger.info("Adding Color " + AbstractCardEnum.CRYSTAL.toString());
+    public CrystalMod()
+    {
+	logger.info("Adding Color " + AbstractCardEnum.CRYSTAL.toString());
 
-		BaseMod.addColor(AbstractCardEnum.CRYSTAL, PURPLE, ATTACK, SKILL, POWER, ENERGY_ORB, ATTACK_PORTRAIT,
-				SKILL_PORTRAIT, POWER_PORTRAIT, ENERGY_ORB_PORTRAIT, CARD_ENERGY_ORB);
-	}
+	BaseMod.addColor(AbstractCardEnum.CRYSTAL, PURPLE, ATTACK, SKILL, POWER, ENERGY_ORB, ATTACK_PORTRAIT,
+		SKILL_PORTRAIT, POWER_PORTRAIT, ENERGY_ORB_PORTRAIT, CARD_ENERGY_ORB);
+    }
 
-	@Override
-	public void receiveEditCharacters() {
-		logger.info("Adding Character " + PlayerClassEnum.CRYSTAL.toString());
+    @Override
+    public void receiveEditCharacters()
+    {
+	logger.info("Adding Character " + PlayerClassEnum.CRYSTAL.toString());
 
-		BaseMod.addCharacter(new Crystal(CardCrawlGame.playerName), BUTTON, PORTRAIT, PlayerClassEnum.CRYSTAL);
-	}
+	BaseMod.addCharacter(new Crystal(CardCrawlGame.playerName), BUTTON, PORTRAIT, PlayerClassEnum.CRYSTAL);
+    }
 
-	@Override
-	public void receiveEditRelics() {
+    @Override
+    public void receiveEditRelics()
+    {
 
-	}
+    }
 
-	@Override
-	public void receiveEditCards() {
-		BaseMod.addCard(new Revolver_Crystal());
-		BaseMod.addCard(new CrystalShield());
-		BaseMod.addCard(new Shield_Crystal());
-		BaseMod.addCard(new CrystalShards());
-	}
+    @Override
+    public void receiveEditCards()
+    {
+	BaseMod.addCard(new Revolver_Crystal());
+	BaseMod.addCard(new CrystalShield());
+	BaseMod.addCard(new Shield_Crystal());
+	BaseMod.addCard(new CrystalShards());
+    }
 
-	@Override
-	public void receiveEditStrings() {
-		String relicStrings = Gdx.files.internal(RELIC_STRINGS).readString(String.valueOf(StandardCharsets.UTF_8));
-		BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
+    @Override
+    public void receiveEditStrings()
+    {
+	String relicStrings = Gdx.files.internal(RELIC_STRINGS).readString(String.valueOf(StandardCharsets.UTF_8));
+	BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
 
-		String cardStrings = Gdx.files.internal(CARD_STRINGS).readString(String.valueOf(StandardCharsets.UTF_8));
-		BaseMod.loadCustomStrings(CardStrings.class, cardStrings);
-	}
+	String cardStrings = Gdx.files.internal(CARD_STRINGS).readString(String.valueOf(StandardCharsets.UTF_8));
+	BaseMod.loadCustomStrings(CardStrings.class, cardStrings);
 
-	@Override
-	public void receiveEditKeywords() {
+	String powerStrings = Gdx.files.internal(POWER_STRINGS).readString(String.valueOf(StandardCharsets.UTF_8));
+	BaseMod.loadCustomStrings(PowerStrings.class, powerStrings);
+    }
 
-	}
+    @Override
+    public void receiveEditKeywords()
+    {
 
-	@Override
-	public void receiveCardUsed(AbstractCard c) {
+    }
 
-	}
+    @Override
+    public void receiveCardUsed(AbstractCard c)
+    {
 
-	@Override
-	public void receivePowersModified() {
+    }
 
-	}
+    @Override
+    public void receivePowersModified()
+    {
 
-	@Override
-	public void receivePostBattle(AbstractRoom battleRoom) {
+    }
 
-	}
+    @Override
+    public void receivePostBattle(AbstractRoom battleRoom)
+    {
 
-	@Override
-	public void receivePostDungeonInitialize() {
+    }
 
-	}
+    @Override
+    public void receivePostDungeonInitialize()
+    {
 
-	@Override
-	public void receivePostExhaust(AbstractCard c) {
+    }
 
-	}
+    @Override
+    public void receivePostExhaust(AbstractCard c)
+    {
 
-	@Override
-	public void receivePostDraw(AbstractCard c) {
+    }
 
-	}
+    @Override
+    public void receivePostDraw(AbstractCard c)
+    {
 
-	@Override
-	public boolean receivePreMonsterTurn(AbstractMonster m) {
-		return false;
-	}
+    }
+
+    @Override
+    public boolean receivePreMonsterTurn(AbstractMonster m)
+    {
+	return false;
+    }
 
 }
--- src/com/flash/ns/characters/crystal/cards/Shield_Crystal.java
+++ src/com/flash/ns/characters/crystal/cards/Shield_Crystal.java
@@ -17,74 +17,80 @@ import basemod.abstracts.CustomCard;
  * @version 0.0.1
  *
  */
-public class Shield_Crystal extends CustomCard {
-	/**
-	 * The card ID.
-	 */
-	public static final String ID = "Shield_Crystal";
+public class Shield_Crystal extends CustomCard
+{
+    /**
+     * The card ID.
+     */
+    public static final String ID = "Shield_Crystal";
 
-	/**
-	 * The card's CardString.
-	 */
-	private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    /**
+     * The card's CardString.
+     */
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-	/**
-	 * The name of the card.
-	 */
-	public static final String NAME = cardStrings.NAME;
+    /**
+     * The name of the card.
+     */
+    public static final String NAME = cardStrings.NAME;
 
-	/**
-	 * The description of the card.
-	 */
-	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    /**
+     * The description of the card.
+     */
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
-	/**
-	 * The image the card will display.
-	 */
-	public static final String IMG_PATH = "images/cards/placeholder.png";
+    /**
+     * The image the card will display.
+     */
+    public static final String IMG_PATH = "images/cards/placeholder.png";
 
-	/**
-	 * The amount of energy the card will use.<br>
-	 * If the value is negative one then it'll consume as much energy as possible.
-	 */
-	public static final int COST = 1;
-	
-	public static final int UPGRADE_PLUS_BLOCK = 3;
-	public static final int BLOCK = 5;
+    /**
+     * The amount of energy the card will use.<br>
+     * If the value is negative one then it'll consume as much energy as possible.
+     */
+    public static final int COST = 1;
 
-	/**
-	 * Class constructor.
-	 */
-	public Shield_Crystal() {
-		super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, AbstractCardEnum.CRYSTAL, CardRarity.BASIC,
-				CardTarget.SELF);
-		block = baseBlock = BLOCK;
+    public static final int UPGRADE_PLUS_BLOCK = 3;
+    public static final int BLOCK = 5;
+
+    /**
+     * Class constructor.
+     */
+    public Shield_Crystal()
+    {
+	super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, AbstractCardEnum.CRYSTAL, CardRarity.BASIC,
+		CardTarget.SELF);
+	block = baseBlock = BLOCK;
+    }
+
+    /**
+     * Called to make a copy of the card.
+     */
+    @Override
+    public AbstractCard makeCopy()
+    {
+	return new Shield_Crystal();
+    }
+
+    /**
+     * Called to upgrade the card.
+     */
+    @Override
+    public void upgrade()
+    {
+	if (!upgraded)
+	{
+	    upgradeName();
+	    upgradeBlock(UPGRADE_PLUS_BLOCK);
 	}
+    }
 
-	/**
-	 * Called to make a copy of the card.
-	 */
-	@Override
-	public AbstractCard makeCopy() {
-		return new Shield_Crystal();
-	}
-
-	/**
-	 * Called to upgrade the card.
-	 */
-	@Override
-	public void upgrade() {
-		if (!upgraded) {
-			upgradeName();
-			upgradeBlock(UPGRADE_PLUS_BLOCK);
-		}
-	}
-
-	/**
-	 * Called when the card is played.
-	 */
-	@Override
-	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
-	}
+    /**
+     * Called when the card is played.
+     */
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+	AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
+    }
 }
